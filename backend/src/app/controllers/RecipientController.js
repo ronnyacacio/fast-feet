40eb456a3b78328e478b53e17645f32213413daf
Ronny Ac[acio <ronnyacacio27@gmail.com>
2020-04-08 17:23:55 -0300

    feat: method update recipients

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -41,6 +41,49 @@ class RecipientController {
       cep,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      district: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ error: 'Validation fails' });
+
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (!recipient)
+      return res.status(400).json({ error: "Recipient don't exists" });
+
+    const {
+      name,
+      state,
+      city,
+      district,
+      street,
+      number,
+      complement,
+      cep,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      name,
+      state,
+      city,
+      district,
+      street,
+      number,
+      complement,
+      cep,
+    });
+  }
 }
 
 export default new RecipientController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,5 +12,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
