4e1600f82b7ad46e6a335a0ecf89e184ace92be4
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-30 13:42:59 -0300

    styles page SignIn

--- web/src/layouts/auth/styles.js
+++ web/src/layouts/auth/styles.js
@@ -8,4 +8,54 @@ export const Wrapper = styled.div`
   align-items: center;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  text-align: center;
+  background: #fff;
+  padding: 60px 30px;
+  border-radius: 4px;
+
+  img {
+    width: 250;
+    height: 45px;
+    padding: 0 10px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+  }
+
+  span {
+    font-size: 14px;
+    color: #444;
+    font-weight: bold;
+    align-self: flex-start;
+    margin-bottom: 10px;
+  }
+
+  input {
+    font-size: 16px;
+    background: #fff;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    height: 45px;
+    padding: 0 15px;
+    color: #333;
+    margin: 0 0 10px;
+
+    &::placeholder {
+      color: #999;
+    }
+  }
+
+  button {
+    background: #7d40e7;
+    border-radius: 4px;
+    height: 45px;
+    color: #fff;
+    font-size: 16px;
+    font-weight: bold;
+    border: 0;
+  }
+`;
--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  function handleSubmit() {
+    alert('Entrou!');
+  }
+
+  return (
+    <>
+      <img src={logo} alt="FastFeet" />
+      <Form onSubmit={handleSubmit}>
+        <span>SEU E-MAIL</span>
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
+        <span>SUA SENHA</span>
+        <Input name="password" type="password" placeholder="*************" />
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
