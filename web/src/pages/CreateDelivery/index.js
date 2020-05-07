ba120f8f3dc526b40881e0ac4adc33749291b42b
Ronny Acacio <ronnyacacio27@gmail.com>
2020-05-07 20:25:56 -0300

    initial create deliveries

--- web/src/pages/CreateDelivery/index.js
+++ web/src/pages/CreateDelivery/index.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import AsyncSelect from 'react-select/async';
+import { MdKeyboardArrowLeft, MdDone } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Record, Actions, Content } from './styles';
+
+const options = ['Ronny', 'Renato', 'Renan', 'Rodrigo'];
+
+const filterColors = (inputValue) => {
+  return options.filter((i) =>
+    i.toLowerCase().includes(inputValue.toLowerCase())
+  );
+};
+
+const promiseOptions = (inputValue) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(filterColors(inputValue));
+    }, 1000);
+  });
 
 export default function CreateDelivery() {
-  return <div />;
+  return (
+    <Record>
+      <header>
+        <strong>Cadastro de encomendas</strong>
+        <Actions>
+          <Link to="/delivery">
+            <button className="back" type="button">
+              <MdKeyboardArrowLeft color="#fff" size={22} />
+              VOLTAR
+            </button>
+          </Link>
+          <button className="save" type="submit">
+            <MdDone color="#fff" size={22} />
+            SALVAR
+          </button>
+        </Actions>
+      </header>
+      <Content>
+        <div>
+          <AsyncSelect
+            cacheOptions
+            defaultOptions
+            placeholder="Destinatário"
+            loadOptions={promiseOptions}
+          />
+        </div>
+      </Content>
+    </Record>
+  );
 }
---
+++ web/src/pages/CreateDelivery/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import { Form } from '@rocketseat/unform';
+
+export const Record = styled(Form)`
+  padding: 30px 250px;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 30px;
+    strong {
+      font-size: 24px;
+      color: #444;
+    }
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    border-radius: 4px;
+    color: #fff;
+    font-weight: bold;
+    padding: 7px 15px;
+  }
+
+  .back {
+    background: #ccc;
+    margin-right: 20px;
+  }
+
+  .save {
+    background: #7d40e7;
+  }
+`;
+
+export const Content = styled.div`
+  background: #fff;
+  flex: 1;
+  height: 300px;
+`;
